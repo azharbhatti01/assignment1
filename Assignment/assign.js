df04290b04dffd8f70250b60a5afae526d60c1ad
azharbhatti01 <139690627+azharbhatti01@users.noreply.github.com>
2023-08-06 00:01:11 +0500

    Add files via upload

---
+++ Assignment/assign.js
@@ -0,0 +1,4 @@
+"use strict";
+let personName = "azhar";
+let message = `hello ${personName} i hope you undertand `;
+console.log(message);
---
+++ Assignment/assign.ts
@@ -0,0 +1,3 @@
+let personName:string = "azhar";
+let message: string= `hello ${personName} i hope you undertand `;
+console.log(message);
